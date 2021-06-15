40c51e8584585a15d421e529fbc55077a090c7bd
Tamara <Tamara@LAPTOP-KFHEUE2T>
2021-06-15 17:45:32 +0200

    Subida1

---
+++ EV2-Junio-TR/src/Ev2Junio2/Junio.java
@@ -0,0 +1,20 @@
+package Ev2Junio2;
+
+public class Junio {
+	public int miMetodo(int numero1, int numero2) {
+		int resultado = metodoTR(numero1, numero2);
+		return resultado;
+		}
+
+	public int metodoTR(int numero1, int numero2) {
+		int resultado = 0;
+		if (numero1 < 5) {
+		resultado = 55;
+		} else if (numero2 > 10) {
+		resultado = 100;
+		}
+		return resultado;
+	}
+		
+	
+}
